cd3aa8276f31ece9ff7182ec1114817d370b77e2
moklick <info@moritzklack.com>
2018-03-12 00:44:50 +0100

    rename numOfScales to octaves

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const scribble = require('scribbletune');
 
 // example data
-const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
 const min = Math.min(...data);
-const numberOfScales = [...Array(5)].map((d, i) => i + 1); // [1, 2, 3, 4, 5]
+const octaves = [...Array(5)].map((d, i) => i + 1); // [1, 2, 3, 4, 5]
 
 // creates array of notes like 'c1', 'd1', 'e1', 'gb1', 'ab1', 'bb1', 'c2', ...
-const notes = numberOfScales.reduce((res, num) =>
-  res.concat(scribble.scale('c', 'whole tone', num, false))
+const notes = octaves.reduce((res, octave) =>
+  res.concat(scribble.scale('c', 'whole tone', octave, false))
 , []);
 
 const midiData = scribble.clip({
