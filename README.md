3d269f9c9f02ce9e529bb59a709989143388ac09
moklick <info@moritzklack.com>
2018-03-11 19:44:48 +0100

    init 🎶

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# from-data-to-midi
-simple Node.js script to transform data to a MIDI file
+# From Data to Sound
+
+Simple Node.js script to transform an array of values to a MIDI file.
+
+## Installation
+
+```
+npm install
+```
+
+## Run script
+
+```
+npm start
+```
+
+### Requirements
+
+* [Node.js](https://nodejs.org/en/)
+* [npm](https://www.npmjs.com/)
+
+If you are new to Node.js, we recommend to use [nvm](https://github.com/creationix/nvm) for the installation.
+
---
+++ index.js
@@ -0,0 +1,21 @@
+const scribble = require('scribbletune');
+
+// example data
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+const min = Math.min(...data);
+const numberOfScales = [...Array(5)].map((d, i) => i + 1); // [1, 2, 3, 4, 5]
+const scale = ['c', 'd', 'e', 'f#', 'g#', 'a#'];
+
+// creates array of notes like 'c1', 'd1', 'e1', 'f#1', 'g#1', 'a#1', 'c2', 'd2', ...
+const notes = numberOfScales.reduce((res, num) =>
+  res.concat(scale.map(note => `${note}${num + 1}`))
+, []);
+
+const midiData = scribble.clip({
+  notes: data.map(value => notes[value - min]),
+  pattern: 'x',
+  noteLength: '1/16',
+});
+
+scribble.midi(midiData, 'data-sonification.mid');
