15d2a505429ba60de77f90a2bb57cbb7bdd568a9
moklick <info@moritzklack.com>
2018-03-11 21:07:33 +0100

    use scribbletune mode function 🔈

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
 const min = Math.min(...data);
 const numberOfScales = [...Array(5)].map((d, i) => i + 1); // [1, 2, 3, 4, 5]
-const scale = ['c', 'd', 'e', 'f#', 'g#', 'a#'];
 
-// creates array of notes like 'c1', 'd1', 'e1', 'f#1', 'g#1', 'a#1', 'c2', 'd2', ...
+// creates array of notes like 'c1', 'd1', 'e1', 'gb1', 'ab1', 'bb1', 'c2', ...
 const notes = numberOfScales.reduce((res, num) =>
-  res.concat(scale.map(note => `${note}${num}`))
+  res.concat(scribble.mode('c', 'whole tone', num, false))
 , []);
 
 const midiData = scribble.clip({
